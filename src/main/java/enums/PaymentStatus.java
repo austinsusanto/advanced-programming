5d42d1bdc3ce7cfcb95515227d035205b4a3b1b4
Austin <austinsusanto@gmail.com>
2024-03-22 11:04:47 +0700

    [GREEN] Added test for setStatus & setMethod on Payment test

--- src/main/java/enums/PaymentStatus.java
+++ src/main/java/enums/PaymentStatus.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum PaymentStatus {
     SUCCESS("SUCCESS"),
-    FAILED("FAILED");
+    REJECTED("REJECTED");
 
     private final String value;
     private PaymentStatus(String value) {
--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -19,7 +19,7 @@ public class PaymentTest {
     }
 
     @Test
-    void testCreateValidPayment() {
+    void testCreateValidPaymentStatus() {
         paymentData.put("voucherCode", "ESHOP1234ABC5678");
         Payment payment = new Payment(
                 "13652556-012a-4c07-b546-54eb1396d79b",
@@ -29,6 +29,18 @@ public class PaymentTest {
         );
 
         assertEquals(PaymentStatus.SUCCESS.getValue(), payment.getStatus());
+    }
+
+    @Test
+    void testCreateValidPaymentMethod() {
+        paymentData.put("voucherCode", "ESHOP1234ABC5678");
+        Payment payment = new Payment(
+                "13652556-012a-4c07-b546-54eb1396d79b",
+                PaymentMethod.VOUCHER.getValue(),
+                PaymentStatus.SUCCESS.getValue(),
+                paymentData
+        );
+
         assertEquals(PaymentMethod.VOUCHER.getValue(), payment.getMethod());
     }
 
@@ -59,4 +71,63 @@ public class PaymentTest {
             );
         });
     }
+
+    @Test
+    void testSetValidPaymentStatus() {
+        paymentData.put("voucherCode", "ESHOP1234ABC5678");
+        Payment payment = new Payment(
+                "13652556-012a-4c07-b546-54eb1396d79b",
+                PaymentMethod.VOUCHER.getValue(),
+                PaymentStatus.SUCCESS.getValue(),
+                paymentData
+        );
+        payment.setStatus(PaymentStatus.REJECTED.getValue());
+
+        assertEquals(PaymentStatus.REJECTED.getValue(), payment.getStatus());
+    }
+
+    @Test
+    void testSetInvalidPaymentStatus() {
+        paymentData.put("voucherCode", "ESHOP1234ABC5678");
+        Payment payment = new Payment(
+                "13652556-012a-4c07-b546-54eb1396d79b",
+                PaymentMethod.VOUCHER.getValue(),
+                PaymentStatus.SUCCESS.getValue(),
+                paymentData
+        );
+
+        assertThrows(IllegalArgumentException.class, () -> {
+           payment.setStatus("--INVALID PAYMENT STATUS--");
+        });
+    }
+
+    @Test
+    void testSetValidPaymentMethod() {
+        paymentData.put("voucherCode", "ESHOP1234ABC5678");
+        Payment payment = new Payment(
+                "13652556-012a-4c07-b546-54eb1396d79b",
+                PaymentMethod.VOUCHER.getValue(),
+                PaymentStatus.SUCCESS.getValue(),
+                paymentData
+        );
+        payment.setMethod(PaymentMethod.TRANSFER.getValue());
+
+        assertEquals(PaymentMethod.TRANSFER.getValue(), payment.getMethod());
+    }
+
+    @Test
+    void testSetInvalidPaymentMethod() {
+        paymentData.put("voucherCode", "ESHOP1234ABC5678");
+        Payment payment = new Payment(
+                "13652556-012a-4c07-b546-54eb1396d79b",
+                PaymentMethod.VOUCHER.getValue(),
+                PaymentStatus.SUCCESS.getValue(),
+                paymentData
+        );
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            payment.setMethod("--INVALID PAYMENT METHOD--");
+        });
+    }
+
 }
